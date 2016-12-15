22988be19b6f75ca08639017bfcf7b54fdb6ef3e
Adrian Boisclair <aboisclair@zoemg.com>
2016-12-15 11:48:36 -0800

    fix errors and reformat for readability

--- js/script.js
+++ js/script.js
@@ -4,80 +4,137 @@
 var $navItem = $('#myNavbar a');
 
 //When a user clicks a nav item...
-$navItem.on('click', function(event) {
+$navItem.on('click', function (event) {
 
-	// if there is a hash tag
-	if (this.hash !== "") {
+    // if there is a hash tag
+    if (this.hash !== "") {
 
-		// stop the normal link from firing
-		event.preventDefault();
+        // stop the normal link from firing
+        event.preventDefault();
 
-		// store the hash tag in a local variable
-		var hash = this.hash;
+        // store the hash tag in a local variable
+        var hash = this.hash;
 
-		// scroll the document from the current location to the location of the hastag.
-		$('html, body').animate({
+        // scroll the document from the current location to the location of the hastag.
+        $('html, body').animate({
 
-			scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top
 
-		}, 800, function(){
+        }, 800, function () {
 
-			window.location.hash = hash;  // Update the hash tag in the location bar
+            window.location.hash = hash;  // Update the hash tag in the location bar
 
-		});
-	}
+        });
+    }
 });
 
 // GOOGLE MAP EMBED
-    google.maps.event.addDomListener(window, 'load', init);
-    var map;
-    function init() {
-        var mapOptions = {
-            center: new google.maps.LatLng(34.147151,-118.345901),
-            zoom: 9,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.DEFAULT,
-            },
-            disableDoubleClickZoom: false,
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-            },
-            scaleControl: true,
-            scrollwheel: false,
-            panControl: true,
-            streetViewControl: true,
-            draggable : true,
-            overviewMapControl: true,
-            overviewMapControlOptions: {
-                opened: false,
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}],
+google.maps.event.addDomListener(window, 'load', init);
+function init() {
+    var description, telephone, email, web, markericon, marker, link;
+    var mapOptions = {
+        center: new google.maps.LatLng(34.147151, -118.345901),
+        zoom: 9,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },
+        disableDoubleClickZoom: false,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        },
+        scaleControl: true,
+        scrollwheel: false,
+        panControl: true,
+        streetViewControl: true,
+        draggable: true,
+        overviewMapControl: true,
+        overviewMapControlOptions: {
+            opened: false,
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{
+            "featureType": "administrative.locality",
+            "elementType": "all",
+            "stylers": [{"hue": "#2c2e33"}, {"saturation": 7}, {"lightness": 19}, {"visibility": "on"}]
+        }, {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [{"hue": "#ffffff"}, {"saturation": -100}, {"lightness": 100}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [{"hue": "#ffffff"}, {"saturation": -100}, {"lightness": 100}, {"visibility": "off"}]
+        }, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{"hue": "#bbc0c4"}, {"saturation": -93}, {"lightness": 31}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [{"hue": "#bbc0c4"}, {"saturation": -93}, {"lightness": 31}, {"visibility": "on"}]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "labels",
+            "stylers": [{"hue": "#bbc0c4"}, {"saturation": -93}, {"lightness": -2}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{"hue": "#e9ebed"}, {"saturation": -90}, {"lightness": -8}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [{"hue": "#e9ebed"}, {"saturation": 10}, {"lightness": 69}, {"visibility": "on"}]
+        }, {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [{"hue": "#e9ebed"}, {"saturation": -78}, {"lightness": 67}, {"visibility": "simplified"}]
+        }]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var locations = [
+        ['Me :)', 'undefined', 'undefined', 'undefined', 'undefined', 34.23740947418436, -118.52817026441801, 'https://mapbuildr.com/assets/img/markers/solid-pin-red.png']
+    ];
+    for ( var i = 0; i < locations.length; i++) {
+        if (locations[i][1] == 'undefined') {
+            description = '';
+        } else {
+            description = locations[i][1];
         }
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var locations = [
-['Me :)', 'undefined', 'undefined', 'undefined', 'undefined', 34.23740947418436, -118.52817026441801, 'https://mapbuildr.com/assets/img/markers/solid-pin-red.png']
-        ];
-        for (i = 0; i < locations.length; i++) {
-			if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-			if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-			if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-           if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-           if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-            marker = new google.maps.Marker({
-                icon: markericon,
-                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-                map: map,
-                title: locations[i][0],
-                desc: description,
-                tel: telephone,
-                email: email,
-                web: web
-            });
-link = '';     }
+        if (locations[i][2] == 'undefined') {
+            telephone = '';
+        } else {
+            telephone = locations[i][2];
+        }
+        if (locations[i][3] == 'undefined') {
+            email = '';
+        } else {
+            email = locations[i][3];
+        }
+        if (locations[i][4] == 'undefined') {
+            web = '';
+        } else {
+            web = locations[i][4];
+        }
+        if (locations[i][7] == 'undefined') {
+            markericon = '';
+        } else {
+            markericon = locations[i][7];
+        }
+        marker = new google.maps.Marker({
+            icon: markericon,
+            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+            map: map,
+            title: locations[i][0],
+            desc: description,
+            tel: telephone,
+            email: email,
+            web: web
+        });
+        link = '';
+    }
 
 }
 
